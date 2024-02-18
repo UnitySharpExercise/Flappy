34fbbc0b28a5a009e9cf8b02ab2c4609f1234739
Bobateaaa <153890630+Bobateaaa@users.noreply.github.com>
2024-02-18 14:13:29 -0500

    partie 2.b flappy
    Contrôle des déplacements de Flappy

--- Assets/Sprites/ControleFlappy.cs
+++ Assets/Sprites/ControleFlappy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ControleFlappy : MonoBehaviour
 {
+    public float vitesseX;
+    public float vitesseY;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class ControleFlappy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey("a") || Input.GetKey("right"))
+        {
+            transform.Translate(vitesseX, 0, 0);
+        }
+        if(Input.GetKey("d") || Input.GetKey("left")){
+            transform.Translate(-vitesseX, 0, 0);
+        }
+        if(Input.GetKeyDown("w") || Input.GetKeyDown("up")){
+            transform.Translate(0, vitesseY, 0);
+        }
         
     }
 }
