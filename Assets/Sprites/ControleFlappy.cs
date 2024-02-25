1a6df6658a1686da18624ed80cb3c3c1f7246517
Bobateaaa <153890630+Bobateaaa@users.noreply.github.com>
2024-02-25 17:56:26 -0500

    Flappy3
    Changement ajoutant la scene pour flappy3

--- Assets/Sprites/ControleFlappy.cs
+++ Assets/Sprites/ControleFlappy.cs
@@ -6,6 +6,14 @@ public class ControleFlappy : MonoBehaviour
 {
     public float vitesseX;
     public float vitesseY;
+    public Sprite flappyBlesse; 
+    public Sprite flappyNormal;
+    public GameObject objetPieceOr;
+    public GameObject objetPackVie;
+    public GameObject objetChampignon;
+    public float deplacementAleatoireY ;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +31,68 @@ public class ControleFlappy : MonoBehaviour
             transform.Translate(-vitesseX, 0, 0);
         }
         if(Input.GetKeyDown("w") || Input.GetKeyDown("up")){
-            transform.Translate(0, vitesseY, 0);
+            vitesseY = 5;
         }
-        
+        else
+        {
+            vitesseY = GetComponent<Rigidbody2D>().velocity.y;
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, vitesseY);
     }
+
+
+          
+    void OnCollisionEnter2D(Collision2D collisionTrue)
+    {
+        //si l'objet touché est ………. alors on ………..
+        if(collisionTrue.gameObject.name == "Colonne")
+        {
+            GetComponent<SpriteRenderer>().sprite = flappyBlesse;
+        }
+        else if(collisionTrue.gameObject.name == "PieceOr")
+        {
+            collisionTrue.gameObject.SetActive(false);
+            Invoke("ActivePieceOr", 5f);
+        }
+        //si l'objet touché est ………. alors on ………..   
+        else if(collisionTrue.gameObject.name == "PackVie")
+        {
+            GetComponent<SpriteRenderer>().sprite = flappyNormal;
+            collisionTrue.gameObject.SetActive(false);
+            Invoke("ActivePackVie", 5f);
+
+        }
+        else if(collisionTrue.gameObject.name == "Champignon")
+        {
+            transform.localScale *= 2f;
+            Invoke("ChampignonDiminueTaille", 5f);
+            collisionTrue.gameObject.SetActive(false);
+            Invoke("ActiveChampignon", 5f);
+
+        }
+    }
+            void ChampignonDiminueTaille()
+            {
+                transform.localScale /= 2f; 
+            }
+            void ActivePieceOr()
+            {
+                objetPieceOr.SetActive(true);
+                float valeurAleatoireY = Random.Range(-deplacementAleatoireY, deplacementAleatoireY);
+                objetPieceOr.transform.position = new Vector2(objetPieceOr.transform.position.x, valeurAleatoireY);
+            }
+            void ActivePackVie()
+            {
+                objetPackVie.SetActive(true);
+                 float valeurAleatoireY = Random.Range(-deplacementAleatoireY, deplacementAleatoireY);
+                objetPackVie.transform.position = new Vector2(objetPackVie.transform.position.x, valeurAleatoireY);
+
+
+            }
+            void ActiveChampignon()
+            {
+                objetChampignon.SetActive(true);
+                float valeurAleatoireY = Random.Range(-deplacementAleatoireY, deplacementAleatoireY);
+                objetChampignon.transform.position = new Vector2(objetChampignon.transform.position.x, valeurAleatoireY);
+            }
 }
